a7c8180bd9eb1d10268c920397c9089eb626fb5c
zhouyang <zhouyang@aeotrade.com>
2017-12-19 13:58:32 +0800

    修改报关配置文件路径错误

--- custom_addons/customs_center/models/customs_declaration.py
+++ custom_addons/customs_center/models/customs_declaration.py
@@ -12,9 +12,9 @@ import uuid
 from ..utils.to_xml_message import delegate_to_xml
 _logger = logging.getLogger(__name__)
 
-RECV_XML_BASE_PATH = config.options['recv_xml_message_path']
-ERROR_XML_BASE_PATH = config.options['error_xml_message_path']
-BAKUP_XML_BASE_PATH = config.options['bakup_xml_message_path']
+RECV_XML_BASE_PATH = config.options.get('recv_xml_message_path')
+ERROR_XML_BASE_PATH = config.options.get('error_xml_message_path')
+BAKUP_XML_BASE_PATH = config.options.get('bakup_xml_message_path')
 
 
 def check_and_mkdir(*path):
