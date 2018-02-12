2ec68ee7c7bbc2fd56b5337dd2abbdba50397464
zhouyang <zhouyang@aeotrade.com>
2018-02-12 10:07:53 +0800

    修改销售模块

--- custom_addons/boyue_sale_extend/models/order.py
+++ custom_addons/boyue_sale_extend/models/order.py
@@ -257,68 +257,6 @@ class Order(models.Model):
                 obj.update({'customer_service': obj.partner_id.customer_service_ids[0].id})
 
 
-    @api.onchange('template_id')
-    def onchange_template_id(self):
-        if not self.template_id:
-            return
-        template = self.template_id.with_context(lang=self.partner_id.lang)
-
-        order_lines = [(5, 0, 0)]
-        for line in template.quote_line:
-            # if self.pricelist_id:
-            #     price = self.pricelist_id.with_context(uom=line.product_uom_id.id).get_product_price(line.product_id, 1, False)
-            # else:
-            price = line.price_unit         # 废掉价格表功能
-
-            data = {
-                'name': line.name,
-                'price_unit': price,
-                'discount': line.discount,
-                'product_uom_qty': line.product_uom_qty,
-                'product_id': line.product_id.id,
-                'quote_currency_id': line.product_id.currency_id,    # 把产品的货币带到询价货币中去
-                'quote_price_unit': price,      # 当产品发生改变时，报价单价也要发生改变
-                'layout_category_id': line.layout_category_id,
-                'product_uom': line.product_uom_id.id,
-                'website_description': line.website_description,
-                'state': 'draft',
-                'customer_lead': self._get_customer_lead(line.product_id.product_tmpl_id),
-            }
-            # if self.pricelist_id:
-            #     data.update(self.env['sale.order.line']._get_purchase_price(self.pricelist_id, line.product_id, line.product_uom_id, fields.Date.context_today(self)))
-            order_lines.append((0, 0, data))
-
-        self.order_line = order_lines
-        self.order_line._compute_tax_id()
-
-        option_lines = []
-        for option in template.options:
-            # if self.pricelist_id:
-            #     price = self.pricelist_id.with_context(uom=option.uom_id.id).get_product_price(option.product_id, 1, False)
-            # else:
-            price = option.price_unit
-            data = {
-                'product_id': option.product_id.id,
-                'layout_category_id': option.layout_category_id,
-                'name': option.name,
-                'quantity': option.quantity,
-                'uom_id': option.uom_id.id,
-                'price_unit': price,
-                'discount': option.discount,
-                'website_description': option.website_description,
-            }
-            option_lines.append((0, 0, data))
-        self.options = option_lines
-
-        if template.number_of_days > 0:
-            self.validity_date = fields.Date.to_string(datetime.now() + timedelta(template.number_of_days))
-
-        self.website_description = template.website_description
-        self.require_payment = template.require_payment
-
-        if template.note:
-            self.note = template.note
-
     @api.onchange('business_type')
     def _change_business_type(self):
         """根据业务类型改变单号规则"""
