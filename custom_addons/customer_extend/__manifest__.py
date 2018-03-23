5a361e173ebd888ff3065eb199caad85fb167e0b
o3 <1403905696@qq.com>
2018-03-23 16:36:16 +0800

    考虑后续做成一个通用模块，把原来在博越销售扩展模块加的客户信息字段，移到此模块中

--- custom_addons/customer_extend/__manifest__.py
+++ custom_addons/customer_extend/__manifest__.py
@@ -1,7 +1,7 @@
 # –*– coding: utf–8 –*–
 {
     'name': 'customer_extend',
-    'description': '扩展客户信息界面',
+    'description': '扩展客户信息界面(考虑后续做成一个通用模块，把原来在博越销售扩展模块加的客户信息字段，移到此模块中)',
     'author': 'ou',
     'depends': ['base','sale','basedata'],
     'data': [
