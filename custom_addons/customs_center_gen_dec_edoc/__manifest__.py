fc4b0550c396c6f98ba55a6d45cd8e02a390beb8
o3 <1403905696@qq.com>
2018-03-23 16:35:47 +0800

    一键生成随附单据功能需要用到客户图章，该模块依赖于customer_extend

--- custom_addons/customs_center_gen_dec_edoc/__manifest__.py
+++ custom_addons/customs_center_gen_dec_edoc/__manifest__.py
@@ -2,8 +2,8 @@
 {
     'name': 'customs_center_gen_dec_edoc',
     'description': '关务中心-报关单-添加一键自动生成随附单据功能',
-    'author': 'ou',
-    'depends': ['base','customs_center'],
+    'author': 'ouyubin@aeotrade.com',
+    'depends': ['base','customs_center','customer_extend'],
     'data': [
         "views/customs_declaration_extend_gen_dec_edoc.xml",
         "reports/customs_dec_edoc_report.xml",
