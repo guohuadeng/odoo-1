c8577cbae7f11f52fdc4e91b21dc3e7a0210c4fb
o3 <1403905696@qq.com>
2018-03-24 15:07:07 +0800

    规格型号窗口，添加预览和字符数提示，并修复某些情况下出现的，新开窗口的规格型号和上一次录入的规格型号合并的问题
    1、规格型号窗口，添加预览和字符数提示
    2、修复bug，在商品归类界面（报关单界面无此bug），新建一个商品归类，输入规格型号 0|0|1|1，保存后，再新建一个商品，发现规格型号默认等于了上一次录入的信息 0|0|1|1，重新输入时会进行两次结果累加（即获取到了上一次未销毁的模态窗录入的文本框信息+这一次的模态窗文本框录入的信息）

--- custom_addons/customs_center/static/src/js/widget.js
+++ custom_addons/customs_center/static/src/js/widget.js
@@ -3,6 +3,8 @@ odoo.define('customs_center', function (require) {
     var FormWidget = require('web.form_widgets');
     var QWeb = core.qweb;
     var Model = require('web.Model');
+    var result_goods_model = '';//规格型号录入结果
+    var modal_id='';//bootstrap模态框id
 
     QWeb.add_template('/customs_center/static/src/xml/declare_element_modal.xml');
 
@@ -13,17 +15,12 @@ odoo.define('customs_center', function (require) {
             this.set({'tariff': false});
             var tariff_field = this.options && this.options.tariff;
             if (tariff_field) {
-                this.field_manager.on("field_changed:"+tariff_field, this, function () {
-                    this.set({'tariff': this.field_manager.get_field_value(''+tariff_field)})
+                this.field_manager.on("field_changed:" + tariff_field, this, function () {
+                    this.set({'tariff': this.field_manager.get_field_value('' + tariff_field)})
                 })
             }
             this.on('change:tariff', this, this.get_element_name);
             this.element_names = [];
-
-             this.input_temp = '<div class="row" style="padding-bottom: 20px">' +
-                '<div class="col-md-2"><label for="<%= field_id %>"><%= name %></label></div>' +
-                '<div class="col-md-6"><input class="o_form_label" id="<%= field_id %>"/></div>' +
-                +'</div>'
 
         },
 
@@ -32,101 +29,107 @@ odoo.define('customs_center', function (require) {
         },
 
         renderElement: function () {
-          this._super();
-          var self = this;
-          this.modal_id = _.uniqueId("dec-modal-");
-          var temp = $(QWeb.render("declare_element_modal"));
-          temp.prop('id', this.modal_id);
-          setTimeout(function () {
-              $('body').append(temp);
-              $('#'+self.modal_id+' button.oe_highlight').on('click', self, self.generate_string)
-          }, 1000);
+            this._super();
+            var self = this;
+            this.modal_id = _.uniqueId("dec-modal-");
+
+            modal_id=this.modal_id;
+
+            var temp = $(QWeb.render("declare_element_modal"));
+            temp.prop('id', this.modal_id);
+            setTimeout(function () {
+                $('body').append(temp);
+                $('#' + modal_id + ' button.oe_highlight').on('click', self, self.submit);//点击确定时，根据窗口中各申报要素录入结果，计算出规格型号，填到规格型号文本框中
+            }, 1000);
 
         },
 
         destroy: function () {
-            $('#'+this.modal_id).remove();
-            this._super.apply(this, arguments)
+            console.log("destroy");
+
         },
 
         show_ele_modal: function () {
-            $('#' + this.modal_id).modal();
-            var self = this;
-            var display_board = $('#'+self.modal_id+' #declare-element-names');
+            $('#' + modal_id).modal();
+            var display_board = $('#' + modal_id + ' #declare-element-names');
             display_board.empty();
+            var self = this;
             var input_tags = [];
             //console.log(self.element_names);
             _.each(self.element_names, function (element_name) {
                 var ele_name = element_name[1];
                 var sequence = element_name[0];
-                var temp = _.template(self.input_temp);
-                var field = temp({field_id: 'dec-ele-'+sequence, name: ele_name});
-                //var input_tag = $(field);
-
 
                 var input_tag;
-                if(ele_name=="品牌类型"){
-                    var select_pingpai='<select class="col-md-6" id="select_dec_ele_0" onchange="$(\'#dec-ele-0\').val(this.value)">' +
-                    '<option value="0">0-无品牌</option>' +
+                // 要素名为品牌类型、出口享惠情况时，比较特殊，需要用下拉框选择，其他要素可以直接用文本框
+                if (ele_name == "品牌类型") {
+                    var select_brand = '<select class="col-md-6" id="select_dec_ele_0" onchange="$(\'#dec-ele-0\').val(this.value)">' +
+                        '<option value="0">0-无品牌</option>' +
                         '<option value="1">1-境内自主品牌</option>' +
                         '<option value="2">2-境内收购品牌</option>' +
                         '<option value="3">3-境外品牌(贴牌生产)</option>' +
                         '<option value="4">4-境外品牌(其他)</option>' +
-                    '</select>';
+                        '</select>';
 
-                    input_tag=$('<div class="row" style="padding-bottom: 20px">'
-                    +'<div class="col-md-2"><label for="'+'dec-ele-'+sequence+'">'+ele_name+'</label></div>'
-                    +'<div class="col-md-8"><input class="col-md-1" value="0" readonly="true"  id="'+'dec-ele-'+sequence+'"/>'+select_pingpai+'</div>'
-                    +'</div>');
-                }else if(ele_name=="出口享惠情况"){
-                    var select_chukou='<select class="col-md-6" id="select_dec_ele_1" onchange="$(\'#dec-ele-1\').val(this.value)">' +
-                    '<option value="0">0-出口货物在最终目的国（地区）不享受优惠关税</option>' +
+                    input_tag = $('<div class="row" style="padding-bottom: 20px">'
+                        + '<div class="col-md-2"><label for="' + 'dec-ele-' + sequence + '">' + ele_name + '</label></div>'
+                        + '<div class="col-md-8"><input  class="col-md-1" value="0" readonly="true"  id="' + 'dec-ele-' + sequence + '"/>' + select_brand + '</div>'
+                        + '</div>');
+                } else if (ele_name == "出口享惠情况") {
+                    var select_out = '<select class="col-md-6" id="select_dec_ele_1" onchange="$(\'#dec-ele-1\').val(this.value)">' +
+                        '<option value="0">0-出口货物在最终目的国（地区）不享受优惠关税</option>' +
                         '<option value="1">1-出口货物在最终目的国（地区）享受优惠关税</option>' +
                         '<option value="2">2-出口货物不能确定在最终目的国（地区）享受优惠关税</option>' +
-                        '<option value="3">3-不适用于进口报关单</option>'+
-                    '</select>';
+                        '<option value="3">3-不适用于进口报关单</option>' +
+                        '</select>';
 
-                    input_tag=$('<div class="row" style="padding-bottom: 20px">'
-                    +'<div class="col-md-2"><label for="'+'dec-ele-'+sequence+'">'+ele_name+'</label></div>'
-                    +'<div class="col-md-8"><input class="col-md-1" value="0" readonly="true"  id="'+'dec-ele-'+sequence+'"/>' +select_chukou+'</div>'
-                    +'</div>');
-                }else{
-                    input_tag=$('<div class="row" style="padding-bottom: 20px">'
-                    +'<div class="col-md-2"><label for="'+'dec-ele-'+sequence+'">'+ele_name+'</label></div>'
-                    +'<div class="col-md-8"><input class="col-md-7" class="o_form_label" id="'+'dec-ele-'+sequence+'"/></div>'
-                    +'</div>');
+                    input_tag = $('<div class="row" style="padding-bottom: 20px">'
+                        + '<div class="col-md-2"><label for="' + 'dec-ele-' + sequence + '">' + ele_name + '</label></div>'
+                        + '<div class="col-md-8"><input  class="col-md-1" value="0" readonly="true"  id="' + 'dec-ele-' + sequence + '"/>' + select_out + '</div>'
+                        + '</div>');
+                } else {
+                    input_tag = $('<div class="row" style="padding-bottom: 20px">'
+                        + '<div class="col-md-2"><label for="' + 'dec-ele-' + sequence + '">' + ele_name + '</label></div>'
+                        + '<div class="col-md-8"><input class="col-md-7" class="o_form_label" id="' + 'dec-ele-' + sequence + '"/></div>'
+                        + '</div>');
                 }
 
                 display_board.append(input_tag);
                 input_tags.push(input_tag)
             });
+
             var input_vals = self.$input.val();
-            if(input_vals)
-            {
+            if (input_vals) {
                 var vals = input_vals.split('|');
                 $.each(input_tags, function (idx, obj) {
-                    obj.find('input').val(vals[idx])
-
-                    if(idx==0){
+                    if (idx == 0) {
                         $("#select_dec_ele_0").val(vals[idx]);
-                    }else if(idx==1){
+                    } else if (idx == 1) {
                         $("#select_dec_ele_1").val(vals[idx]);
+                    } else {
+                        obj.find('input').val(vals[idx])
                     }
+
                 })
             }
-            // console.log(display_board.html());
+
+            $('#declare-element-names input').on('change',self,self.generate_goods_model_preview);
+            $('#declare-element-names select').on('change',self,self.generate_goods_model_preview);
+
+            self.generate_goods_model_preview();
         },
 
         remove_field: function () {
             var display_board = $('#declare-element-names');
-            display_board.empty()
+            display_board.empty();
+
         },
 
         get_element_name: function () {
-            if (this.get('tariff')){
+            if (this.get('tariff')) {
                 var DeclareElement = new Model('declare_element');
                 var self = this;
-                if(self.$input)
+                if (self.$input)
                     self.$input.val('');
                 self.element_names = [];
                 DeclareElement.query(['name', 'sequence']).filter([['cus_goods_tariff_id', '=', self.get('tariff')]])
@@ -140,33 +143,51 @@ odoo.define('customs_center', function (require) {
             }
         },
 
-        generate_string: function (event) {
+        submit: function (event) {
+            console.log(event);
             var self = event.data;
-            var inputs = $('#declare-element-names input');
-            var values = [];
-            inputs.each(function () {
-               values.push($(this).val());
-            });
-            console.log(values);
-            var target_string = _.reduce(values, function (meno, value,index) {
-               return meno + '|' + value;
-
-            }, '');
-
-            if (target_string.substr(0,1)=='|'){
-                target_string=target_string.substr(1);
-            }
-            // if (target_string){
-            //     target_string += '|';
-            // }
-            self.$input.val(target_string);
-            $('#'+self.modal_id).modal('hide');
+            self.$input.val(result_goods_model);
+            $('#' + modal_id).modal('hide');
 
             //点击确定时，将焦点移动到下一控件
             var inputs = $("input"); // 获取表单中的所有输入框
             var idx = inputs.index(self.$input);
-            if(idx<inputs.length-1){
-                inputs[idx+1].focus();
+            if (idx < inputs.length - 1) {
+                inputs[idx + 1].focus();
+            }
+
+            self.destroy();
+        },
+        generate_goods_model_preview: function (event) {
+            var inputs = $('#' + modal_id+' #declare-element-names input');//避免获取到上一次录入的数据
+            //console.log(inputs);
+            //console.log(modal_id);
+
+            var values = [];
+            inputs.each(function () {
+                values.push($(this).val());
+            });
+
+            result_goods_model = _.reduce(values, function (meno, value, index) {
+                //console.log(index,value)
+                if (index == 0) {
+                    return value;
+                } else {
+                    return meno + '|' + value;
+                }
+
+            }, '');
+
+            $("p[name='goods_model_preview']").text(result_goods_model);
+            $("p[name='goods_model_preview_count']").text(result_goods_model.length + '/255');
+
+            //超出255个字符时标红提醒
+            if (result_goods_model.length > 255) {
+                $("p[name='goods_model_preview']").css('color', 'red');
+                $("p[name='goods_model_preview_count']").css('color', 'red');
+            } else {
+                $("p[name='goods_model_preview']").css('color', 'black');
+                $("p[name='goods_model_preview_count']").css('color', 'black');
             }
         }
     });
