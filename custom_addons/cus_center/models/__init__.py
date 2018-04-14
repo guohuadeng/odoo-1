a0c6552af6b0e7aea47d30b2d21b7816c9693496
zhiqiang <wangzhiqiang@aeotrade.com>
2018-04-14 18:06:00 +0800

    20180414 关务中心 玉斌代码重构之后合并

--- custom_addons/cus_center/models/__init__.py
+++ custom_addons/cus_center/models/__init__.py
@@ -4,7 +4,7 @@
 # from . import customs_receipt, customs_register_company, customs_selection_field, goods_classification, result_status
 # from . import setting_declaration
 
-from . import goods_classification, customs_order_goods_list, customs_order
+from . import goods_classification, customs_order_goods_list, customs_order, customs_dashboard
 from . import edoc_queue, customs_dec_receipt, customs_dec_goods_list, customs_container, customs_selection_field, \
     customs_dec_lic_doc, result_status, customs_dec
 from . import setting_declaration
